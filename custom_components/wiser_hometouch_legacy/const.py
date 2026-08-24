df09cb8de4815fec88287df2e70d2d0aa40d4fb8
Balthazar62 <299246800+BalthazarTech@users.noreply.github.com>
2026-08-24 19:39:48 +0200

    Add integration constants

---
+++ custom_components/wiser_hometouch_legacy/const.py
@@ -0,0 +1,6 @@
+"""Constants for Wiser HomeTouch Legacy."""
+
+DOMAIN = "wiser_hometouch_legacy"
+CONF_HOST = "host"
+DEFAULT_SCAN_INTERVAL = 10
+USER_AGENT = "com.schneider-electric.wiserone/Android-1.0.1"
