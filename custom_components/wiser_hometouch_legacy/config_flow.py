018e48fe0a0025cacdf11d6fdc383576066fe622
Balthazar62 <299246800+BalthazarTech@users.noreply.github.com>
2026-08-26 12:52:38 +0200

    Ajoute la modification de l'adresse IP depuis les options

--- custom_components/wiser_hometouch_legacy/config_flow.py
+++ custom_components/wiser_hometouch_legacy/config_flow.py
@@ -10,6 +10,24 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from .api import HomeTouchApi, HomeTouchError
 from .const import CONF_HOST, DOMAIN
 
+DEFAULT_HOST = "10.0.0.66"
+
+
+async def _async_validate_host(hass, host: str) -> str | None:
+    """Valide qu'une adresse correspond à un HomeTouch compatible."""
+    api = HomeTouchApi(async_get_clientsession(hass), host)
+
+    try:
+        basic = await api.get_basic_moments()
+    except HomeTouchError:
+        return "cannot_connect"
+
+    values = basic.get("sceneValues", [])
+    if not all(moment in values for moment in ("Home", "Away", "Sleep")):
+        return "not_hometouch"
+
+    return None
+
 
 class HomeTouchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Wiser HomeTouch Legacy."""
@@ -22,23 +40,16 @@ class HomeTouchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         if user_input is not None:
             host = user_input[CONF_HOST].strip()
-            api = HomeTouchApi(async_get_clientsession(self.hass), host)
-
-            try:
-                basic = await api.get_basic_moments()
-            except HomeTouchError:
-                errors["base"] = "cannot_connect"
+            error = await _async_validate_host(self.hass, host)
+            if error:
+                errors["base"] = error
             else:
-                values = basic.get("sceneValues", [])
-                if not all(moment in values for moment in ("Home", "Away", "Sleep")):
-                    errors["base"] = "not_hometouch"
-                else:
-                    await self.async_set_unique_id(host)
-                    self._abort_if_unique_id_configured()
-                    return self.async_create_entry(
-                        title=f"Wiser HomeTouch ({host})",
-                        data={CONF_HOST: host},
-                    )
+                await self.async_set_unique_id(host)
+                self._abort_if_unique_id_configured()
+                return self.async_create_entry(
+                    title=f"Wiser HomeTouch ({host})",
+                    data={CONF_HOST: host},
+                )
 
         return self.async_show_form(
             step_id="user",
@@ -46,9 +57,55 @@ class HomeTouchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 {
                     vol.Required(
                         CONF_HOST,
-                        default=user_input.get(CONF_HOST, "10.0.0.66")
+                        default=user_input.get(CONF_HOST, DEFAULT_HOST)
                         if user_input
-                        else "10.0.0.66",
+                        else DEFAULT_HOST,
+                    ): str,
+                }
+            ),
+            errors=errors,
+        )
+
+    @staticmethod
+    def async_get_options_flow(config_entry):
+        """Retourne le flux d'options de l'intégration."""
+        return HomeTouchOptionsFlow(config_entry)
+
+
+class HomeTouchOptionsFlow(config_entries.OptionsFlow):
+    """Permet de modifier les paramètres d'un HomeTouch configuré."""
+
+    def __init__(self, config_entry) -> None:
+        self._config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        """Modifie l'adresse IP du HomeTouch."""
+        errors = {}
+        current_host = self._config_entry.options.get(
+            CONF_HOST,
+            self._config_entry.data[CONF_HOST],
+        )
+
+        if user_input is not None:
+            host = user_input[CONF_HOST].strip()
+            error = await _async_validate_host(self.hass, host)
+            if error:
+                errors["base"] = error
+            else:
+                return self.async_create_entry(
+                    title="",
+                    data={CONF_HOST: host},
+                )
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        CONF_HOST,
+                        default=user_input.get(CONF_HOST, current_host)
+                        if user_input
+                        else current_host,
                     ): str,
                 }
             ),
